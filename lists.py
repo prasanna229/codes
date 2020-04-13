01b2ea993a190752086432a5a5d373da09aea045
prasanna229 <63359889+prasanna229@users.noreply.github.com>
2020-04-13 12:39:03 +0530

    subtting to my captain

---
+++ lists.py
@@ -0,0 +1,13 @@
+
+introduction_sheet =[
+    ["john","mary","sony"],
+    [32 ,45 ,43],
+    ["dancing","playing football","singing"]
+]
+print(f"name:{introduction_sheet[0][0]}")
+print(f"age:{introduction_sheet[1][0]}")
+print(f"hobbies:{introduction_sheet[2][0]}")
+
+print("assinging elements to list")
+list1 =["rio","sara","damon"]
+print(list1.sort())
---
+++ tuples.py
@@ -0,0 +1,5 @@
+coordinates = (22.6,85.4)
+x,y = coordinates
+print(f"lattitude:{x}")
+print(f"longitude:{y}")
+print(f"the length of tuple is:{len(coordinates)}")
