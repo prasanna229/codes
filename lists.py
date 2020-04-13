4cb29437b537915cabf3f9ed7fc5dff0a1dee58d
prasanna229 <63359889+prasanna229@users.noreply.github.com>
2020-04-13 12:41:39 +0530

    Update lists.py

--- lists.py
+++ lists.py
@@ -8,6 +8,4 @@ print(f"name:{introduction_sheet[0][0]}")
 print(f"age:{introduction_sheet[1][0]}")
 print(f"hobbies:{introduction_sheet[2][0]}")
 
-print("assinging elements to list")
-list1 =["rio","sara","damon"]
-print(list1.sort())
+
